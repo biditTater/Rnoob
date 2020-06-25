8be1ebdc6d7bba4d12a085816021e502665c8e28
biditTater <67361519+biditTater@users.noreply.github.com>
2020-06-25 21:00:50 +0545

    ProgrammingAssignment2
    RE-Uploaded and edited

--- cachematrix.R
+++ cachematrix.R
@@ -6,6 +6,7 @@
 ## Write a short comment describing this function
 ## makeCacheMatrix is a function which creates a special "matrix" object that can
 # cache its inverse for the input (which is an invertible square matrix)
+#This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
     j <- NULL
@@ -45,3 +46,44 @@ cacheSolve <- function(x, ...) {
 }
 
 #Output of my Function
+> source('C:/Users/Sony/Desktop/cachematrix.R')
+> my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+> my_matrix$get()
+[,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> my_matrix$getInverse()
+NULL
+> cacheSolve(my_matrix)
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(my_matrix)
+getting cached data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> my_matrix$getInverse()
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+> my_matrix$get()
+[,1] [,2]
+[1,]    2    1
+[2,]    2    4
+> my_matrix$getInverse()
+NULL
+> cacheSolve(my_matrix)
+[,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+> cacheSolve(my_matrix)
+getting cached data
+[,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+> my_matrix$getInverse()
+[,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
